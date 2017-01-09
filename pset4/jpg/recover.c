e1f22b33441cc8c0ae0100d25075776e89ef0fc1
Darren Green <redfox714@gmail.com>
2017-01-09 15:04:07 -0500

    final version
    final version of recover.c.  Tested and passes.  Needed "unsigned char" for buffer instead of "uint8_t".  Unknown reason.

--- pset4/jpg/recover.c
+++ pset4/jpg/recover.c
@@ -2,7 +2,7 @@
  * recover.c
  *
  * by: Darren Green
- * Last Revision 1/3/2016
+ * Last Revision 1/9/2016
  * 
  * Computer Science 50
  * Problem Set 4
@@ -16,8 +16,7 @@
 #include<math.h>
 #include<stdint.h>
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]){
     // ensure proper usage
     if(argc != 1){
         printf("Usage: ./recover\n");
@@ -32,6 +31,7 @@ int main(int argc, char* argv[])
     FILE* card = fopen("card.raw", "r");
     if(card == NULL){
         printf("Could not open card.raw\n");
+        free(filename);
         return 2;
     }
     
@@ -39,65 +39,58 @@ int main(int argc, char* argv[])
     FILE* outfile;
     
     // buffer to read memory card and check signature
-    uint8_t buffer[512] = {0};
+    unsigned char buffer[512] = {0};
 
-    // termination switch
-    int done = 0;
-
+    // check - if file open == 1
+    int open = 0;
+    
     // REPEAT UNTIL END OF CARD
-    do{
-        // read data into buffer from memory card 1-byte 512x times until EOF
-        if(fread(buffer, 1, 512, card) == 512){
-            // START OF A JPG?
-            if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe1){
-        
-                // IF new file is needed, ELSE file is already open
-                if(outfile == NULL){
-                    // make the outfile
-                    sprintf(filename, "%03i.jpg", counter);
+    // read data into buffer from memory card 1-byte 512x times until EOF
+    while(fread(buffer, 1, 512, card) > 0){
+        // START OF A JPG?
+        if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] == 0xe1 || buffer[3] == 0xe0)){
+            // IF new file is needed, ELSE file is already open
+            if(open == 0){
+                // make the outfile
+                sprintf(filename, "%03i.jpg", counter);
             
-                    outfile = fopen(filename, "w");
-                    if(outfile == NULL){
-                        printf("Could not create new JPG file.  Exiting...\n");
-                        fclose(card);
-                        free(filename);
-                        return 3;
-                    }
+                outfile = fopen(filename, "w");
+                if(outfile == NULL){
+                    printf("Could not create new JPG file.  Exiting...\n");
+                    fclose(card);
+                    free(filename);
+                    return 3;
                 }
-                else{
-                    // close the open file
-                    fclose(outfile);
-                    counter++;
-                    
-                    // make a new file to continue
-                    sprintf(filename, "%003i.jpg", counter);
-                    outfile = fopen(filename, "w");
-                    if(outfile == NULL){
-                        printf("Could not create new JPG file.  Exiting...\n");
-                        fclose(card);
-                        free(filename);
-                        return 4;
-                    }
-                }
+                
+                // mark file as open
+                open++;
             }
-        
-            // ALREADY FOUND A JPG?
-            if(outfile != NULL){
-                //  write block to outfile
-                fwrite(buffer, 1, 512, outfile);
+            else{
+                // close the open file
+                fclose(outfile);
+                counter++;
+                    
+                // make a new file to continue
+                sprintf(filename, "%003i.jpg", counter);
+                outfile = fopen(filename, "w");
+                if(outfile == NULL){
+                    printf("Could not create new JPG file.  Exiting...\n");
+                    fclose(card);
+                    free(filename);
+                    return 4;
+                }
             }
         }
-        else{
-            // EOF has been detected - write last bits
-            fwrite(buffer, 1, 512, outfile);
-            
-            // CLOSE ANY REMAINING FILES
-            fclose(card);
-            
-            free(filename);
-            
-            return 0;
+        
+        // ALREADY FOUND A JPG?
+        if(open == 1){
+            //  write block to outfile
+            if(fwrite(buffer, 1, 512, outfile) < 512){
+                // CLOSE ANY REMAINING FILES
+                fclose(card);
+                free(filename);
+                return 0;
+                }
+            }
         }
     }
-    while(done == 0);
-}
